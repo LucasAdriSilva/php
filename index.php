8ca4a9496d299cb8a3a27ef3ecd47e98a9c4f3aa
Lucas Adriano <lucasadrisilva@hotmail.com>
2022-03-02 09:43:25 -0300

    feat(PHP): Ultilizando as primeiras TAGS no PHP

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-teste
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <?php
+    echo 'Ultilizando a tag padrão';
+    ?>
+    <br/>
+    <?= 'Ultilizando a tag impressão' ?>
+
+</body>
+</html>
