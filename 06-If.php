c0caca5f2f3c3b731c45e6a7f84b9f6e4b582d7c
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-03-25 19:29:17 -0300

    feat(PHP):Explorando as funções do IF

--- 06-If.php
+++ 06-If.php
@@ -20,13 +20,49 @@
         > Maior
         <= Menor Igual
         >= Maior Igual
+        AND ou && todas verdadeiras 
+        OR ou || pelo menos uma for verdadeira
+        XOR  verdadeiro apenas se uma das expressoões for verdadeira, mas não ambos
+        ! inverte o resultado
+ 
       */
 
-    if (4 <> 4) {
+    if (!(4 <> 4)) {
         echo  'verdadeiro';
-        echo  'B';
+        echo  '1';
+    }
+    if (4 <> 5 XOR 5 == 9) {
+        echo  'verdadeiro';
+        echo  '2';
+    }
+    if (4 == 4 AND 3 == 4) {
+        echo  'verdadeiro';
+        echo  '3';
+    }
+    if (4 == 4 OR 3 == 4) {
+        echo  'verdadeiro';
+        echo  '4';
     }
 
+    $usuario_cartao = true;
+    $valor = 250;
+    $valor_frete = 50;
+    $recebeu_desconto = false;
+
+    if($usuario_cartao ==true AND $valor >= 100){
+        $valor_frete = 0;
+        $recebeu_desconto = true;
+        echo"<br/> Valor da compra tera desconto no frete e sera = $valor_frete";
+    }
+
+    ?>
+
+    <h3>Informações de pedido</h3>
+
+    <?php
+    if ($recebeu_desconto == true){
+        echo "Seu pedido já está com desconto";
+    }
 
     ?>
 
