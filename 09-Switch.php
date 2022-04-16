c8bb3970fbb77c88286adf1511304ecc7d857fab
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-04-16 16:54:27 -0300

    feat(PHP): Correção switch

--- 09-Switch.php
+++ 09-Switch.php
@@ -1,28 +1,37 @@
 <!DOCTYPE html>
 <html lang="PT-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
+    <?php
     $Nome = "Lucas";
 
-    switch($Nome = "Lucas"){
+    switch ($Nome = "Lucas") {
         case true:
-        echo 'Nome é Lucas';
-        break;
+            echo 'Nome é Lucas';
+            break;
         case false:
-        echo 'O nome não é Lucas';
-        break;
+            echo 'O nome não é Lucas';
+            break;
+        case 'Leadro':
+            echo 'O nome não é Lucas';
+            break;
+        case 1:
+            echo 'O valor é numero 1';
+            break;
 
         default:
-        //
-        break;
+            //
+            break;
     }
 
     ?>
 </body>
+
 </html>
