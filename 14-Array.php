dec4de0283b5ad8b05c1c79f1b57ce216404acc8
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-04-28 13:44:03 -0300

    feat(PHP): Iniciando um Array

---
+++ 14-Array.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    //Sequencias Numericos
+    $lista_frutas = array("Banana","Maça","Motango","Uva");
+    echo '<pre>';
+    var_dump($lista_frutas);
+    echo '</pre>';
+
+    echo "<hr/>";
+
+    echo '<pre>';
+
+    print_r($lista_frutas);
+    echo '</pre>';
+
+    //Associativos
+
+    ?>
+</body>
+
+</html>
