df7ad69819fbd593fd7022cac4fc25023882f80c
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-04-28 11:44:01 -0300

    feat(php): Tratando Data

--- 13-Funções_Nativas_String.php
+++ 13-Funções_Nativas_String.php
@@ -10,6 +10,7 @@
 
 <body>
     <?php
+    /*------------------------------------TEXTO------------------------------------*/ 
     $texto = 'lucas';
      
     echo $texto. " = ";
@@ -30,6 +31,49 @@
     echo $texto. " = ";
     echo substr($texto,2, 4)."<br/> <br/>"; // Captura de um certo trecho
 
+    /*------------------------------------NUMEROS------------------------------------*/ 
+    $num = 3.5;
+
+    echo $num . " = ";
+    echo ceil($num).'<br/>'; //Aredonda pra cima
+
+    echo $num . " = ";
+    echo floor($num).'<br/>'; //Aredonda pra baixo
+
+    echo $num . " = ";
+    echo round($num).'<br/>'; //Aredonda com o valor base nas casas deciamis
+
+    echo "Gera um numero aleatorio = ";
+    echo rand().'<br/>';  // Numero aleatorio
+
+    echo $num . " = ";
+    echo sqrt($num).'<br/><br/><br/><br/>'; // Raiz quadrada
+
+    /*------------------------------------DATA------------------------------------*/ 
+    /*echo date_default_timezone_set('America/Sao_Paulo');
+
+    echo date('d/m/Y  H:i:s')."<br/><br/>";
+
+    echo date_default_timezone_get()."<br/><br/>" ;
+
+    echo date('d/m/Y  H:i:s')."<br/><br/><br/><br/>";*/
+
+    $datainicial = "2018-04-24";
+    $datafinal = "2018-05-15";
+    
+    $timeini = strtotime($datainicial);
+    echo $timeini. "<br/>";
+    echo gettype($timeini). "<br/>";
+
+    $timefin = strtotime($datafinal);
+    
+    echo $timefin. "<br/>";
+    echo gettype($timefin). "<br/>";
+
+    $res2 = $timeini - $datafinal;
+    echo $res2;
+
+   /*strotime(data);*/
 
     ?>
 </body>
